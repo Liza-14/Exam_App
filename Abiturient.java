e626ddf7247b1cb2bd3aff448d4e5a30866fe3a8
Liza <Lizabvb57@gmail.com>
2021-01-11 18:30:18 +0200

    added Abiturient class

---
+++ Abiturient.java
@@ -0,0 +1,10 @@
+public class Abiturient {
+    public String firstName;
+    public String lastName;
+    public int age;
+    public Abiturient(String firstName, String lastname, int age) {
+        this.firstName = firstName;
+        this.lastName = lastname;
+        this.age = age;
+    }
+}
--- App.java
+++ App.java
@@ -1,6 +1,10 @@
 public class App {
     public static void main(String[] args) {
-        System.out.print("Hello World!");
+       Abiturient Liza = new Abiturient("Liza", "Mal", 20);
+       System.out.println(Liza.firstName);
+       System.out.println(Liza.lastName);
+       System.out.println(Liza.age);
+
     }
     
 }
